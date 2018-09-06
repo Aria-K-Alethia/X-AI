a3a750a3a1697f28f685d93572798bc35770d788
Aria-K-Alethia <xindetai@qq.com>
2018-09-06 20:29:47 +0800

    :tada: layer normalization complete

--- layernorm.py
+++ layernorm.py
@@ -3,6 +3,9 @@
 #   implementation of layer normalization
 #   paper: https://arxiv.org/abs/1607.06450v1
 
+import torch
+import torch.nn as nn
+
 class LayerNorm(nn.Module):
 	"""
 	"""
@@ -10,3 +13,9 @@ class LayerNorm(nn.Module):
 		super(LayerNorm, self).__init__()
 		self.size = size
 		self.eps = eps
+		self.a = nn.Parameter(torch.ones(size))
+		self.b = nn.Parameter(torch.zeros(size))
+	def forward(self, x):
+		mean = x.mean(-1, keepdim = True)
+		std = x.std(-1, keepdim = True)
+		return self.a * (x - mean) /  (std + self.eps) + self.b
